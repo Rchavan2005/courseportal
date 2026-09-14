3d2b90145856efa61526b4ef387e350d077b8030
Rchavan2005 <rchavan2803@gmail.com>
2026-09-15 01:56:14 +0530

    allow vercel frontend in cors

--- src/main/java/com/example/courseportalprj/Config/CorsConfig.java
+++ src/main/java/com/example/courseportalprj/Config/CorsConfig.java
@@ -14,7 +14,10 @@ public class CorsConfig {
     public CorsConfigurationSource corsConfigurationSource() {
 
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedOrigins(List.of(
+            "http://localhost:3000",
+            "https://courseportalfront-sand.vercel.app"
+        ));
         configuration.setAllowedMethods(List.of("*"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
